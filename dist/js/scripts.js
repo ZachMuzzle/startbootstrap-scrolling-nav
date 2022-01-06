1dca15a819b4f93d54dd3f448d8ae63fc1476263
ZachMuzzle <zach12342@me.com>
2022-01-05 23:13:17 -0500

    Added photo gallerys and links to  git repos in Projects. Added more images to images folder. Added resume scroll pdf.

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -32,3 +32,29 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+//FOR MODAL IMAGE GALLARY IN PROJECTS SECTION.
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName("myImages");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+//go through all images 
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+img.onclick = function(event){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt; //caption = alt
+}
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
